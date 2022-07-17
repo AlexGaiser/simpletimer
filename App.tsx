2c736bf92cec3fa97c6c6dcd482c56f9d259cbc4
alexgaiser <alexcgaiser@gmail.com>
2022-07-17 17:57:01 -0400

    init project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import Timer from "./components/Timer/Timer";
 
 export default function App() {
+  const [startTime, setStartTime] = useState(0);
+
+  const startTimer = () => {
+    setStartTime(Date.now());
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Timer handleTimeEnd={() => alert("timer ended")} />
+      <Button title="StartButton" onPress={startTimer}>
+        Start
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
